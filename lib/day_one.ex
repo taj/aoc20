ad553d8f9f26fe6fa92072f61a3cc9034cd0e314
Tajinder Chumber <me@taj.dev>
2020-12-01 19:21:16 +0000

    day 1

---
+++ lib/day_one.ex
@@ -0,0 +1,41 @@
+defmodule DayOne do
+  def part_1(), do: read_input() |> do_part_1()
+
+  def part_2(), do: read_input() |> do_part_2()
+
+  # Result is 927684
+  defp do_part_1(input) do
+    matches =
+      for a <- input, b <- input do
+        if a + b == 2020 do
+          a * b
+        end
+      end
+
+    matches
+    |> Enum.reject(&(&1 == nil))
+    |> hd()
+  end
+
+  # Result is 292093004
+  defp do_part_2(input) do
+    matches =
+      for a <- input, b <- input, c <- input do
+        if a + b + c == 2020 do
+          a * b * c
+        end
+      end
+
+    matches
+    |> Enum.reject(&(&1 == nil))
+    |> hd()
+  end
+
+  defp read_input() do
+    "lib/inputs/day_one"
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&String.to_integer/1)
+  end
+end
